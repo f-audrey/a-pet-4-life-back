9fd72cf720ebda54242deb810eed50401009d8c4
Samantha Dangu <dangu.samantha@gmail.com>
2022-03-01 10:05:01 +0100

    fix migration d:m:m (problem with user_review)

--- migrations/Version20220301090109.php
+++ migrations/Version20220301090109.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220225093800 extends AbstractMigration
+final class Version20220301090109 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,6 @@ final class Version20220225093800 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE user_review');
         $this->addSql('ALTER TABLE review ADD user_post_id INT DEFAULT NULL, ADD user_receiver_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C613841D26 FOREIGN KEY (user_post_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C664482423 FOREIGN KEY (user_receiver_id) REFERENCES user (id)');
@@ -31,9 +30,6 @@ final class Version20220225093800 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user_review (user_id INT NOT NULL, review_id INT NOT NULL, INDEX IDX_1C119AFBA76ED395 (user_id), INDEX IDX_1C119AFB3E2E969B (review_id), PRIMARY KEY(user_id, review_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE user_review ADD CONSTRAINT FK_1C119AFB3E2E969B FOREIGN KEY (review_id) REFERENCES review (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE user_review ADD CONSTRAINT FK_1C119AFBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C613841D26');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C664482423');
         $this->addSql('DROP INDEX IDX_794381C613841D26 ON review');
