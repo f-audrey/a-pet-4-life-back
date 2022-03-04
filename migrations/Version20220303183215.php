5fdd9f30c481dc5e6ff7eaffddfe37f233db1618
Samantha Dangu <dangu.samantha@gmail.com>
2022-03-04 09:56:25 +0100

    delete asso species on last migration

--- migrations/Version20220303183215.php
+++ migrations/Version20220303183215.php
@@ -23,16 +23,12 @@ final class Version20220303183215 extends AbstractMigration
         $this->addSql('CREATE TABLE user_species (user_id INT NOT NULL, species_id INT NOT NULL, INDEX IDX_FD02E918A76ED395 (user_id), INDEX IDX_FD02E918B2A1D860 (species_id), PRIMARY KEY(user_id, species_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_species ADD CONSTRAINT FK_FD02E918A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_species ADD CONSTRAINT FK_FD02E918B2A1D860 FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE');
-        $this->addSql('DROP TABLE asso_species');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE asso_species (user_id INT NOT NULL, species_id INT NOT NULL, INDEX IDX_85236B12A76ED395 (user_id), INDEX IDX_85236B12B2A1D860 (species_id), PRIMARY KEY(user_id, species_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE asso_species ADD CONSTRAINT FK_85236B12B2A1D860 FOREIGN KEY (species_id) REFERENCES species (id)');
-        $this->addSql('ALTER TABLE asso_species ADD CONSTRAINT FK_85236B12A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('DROP TABLE user_species');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL');
     }
