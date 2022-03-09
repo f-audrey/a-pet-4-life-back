3a20d7a954bb881b24c66fac44b902a418f0f2ea
Morgane-Rabiller <morganerabiller@yahoo.fr>
2022-03-09 17:51:06 +0100

    modification in form

--- src/Form/AssoType.php
+++ src/Form/AssoType.php
@@ -10,10 +10,13 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Positive;
 
 class AssoType extends AbstractType
@@ -21,16 +24,13 @@ class AssoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('type')
-            ->add('name', TextType::class, ['label' => 'Nom de l\'association','attr' => ['placeholder' => 'saisissez votre nom']])
+            /* ->add('type') */
+            ->add('name', TextType::class, ['label' => 'Nom de l\'association'])
             ->add('siret', IntegerType::class, [
-                'constraints' => [new Positive()],
-                    'attr' => [
-                        'maxlength' => 9
-                        ]
+                'constraints' => [new Length(['min' => 9, 'max' => 9])]
                 ])
-            ->add('mail', EmailType::class, ['label' => 'E-Mail','attr' => ['placeholder' => 'saisissez votre e-mail']])
-            ->add('password', TextType::class, ['label' => 'Mot de passe'])
+            ->add('mail', EmailType::class, ['label' => 'E-Mail'])
+            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
             ->add('adress', TextType::class, ['label' => 'Adresse'])
             ->add('zipcode', TextType::class, ['label' => 'Code postal'])
             ->add('city', TextType::class, ['label' => 'Ville'])
@@ -40,7 +40,7 @@ class AssoType extends AbstractType
             ->add('description', TextareaType::class, ['label' => 'Description'])
             ->add('picture', TextareaType::class, ['label' => 'Photo'])
             ->add('website', TextareaType::class, ['label' => 'Site web'])
-            ->add('roles', CollectionType ::class, ['label' => 'Rôle'])
+            /* ->add('roles', CollectionType ::class, ['label' => 'Rôle']) */
             ->add('species', EntityType::class, [
                 'class' => Species::class,
                 'choice_label' => 'name',
--- src/Form/PartType.php
+++ src/Form/PartType.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,21 +20,13 @@ class PartType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('type')
+            /* ->add('type') */
             ->add('firstname', TextType::class, ['label' => 'prénom','attr' => ['placeholder' => 'saisissez votre Prénom']])
             ->add('lastname', TextType::class, ['label' => 'Nom de famille','attr' => ['placeholder' => 'saisissez votre nom de famille']])
             ->add('mail', EmailType::class, ['label' => 'E-Mail','attr' => ['placeholder' => 'saisissez votre e-mail']])
-            ->add('password', TextType::class, ['label' => 'Mot de passe'])
+            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
             ->add('department', TextType::class, ['label' => 'Département'])
             ->add('phone_number', TextType::class, ['label' => 'Numero de téléphone'])
-            ->add('picture', TextareaType::class, ['label' => 'Photo'])
-            ->add('roles', CollectionType ::class, ['label' => 'Rôle'])
-            ->add('species', EntityType::class, [
-                'class' => Species::class,
-                'choice_label' => 'name',
-                'multiple' => true, 
-                'expanded' => true,
-            ])
             ;
     }
 
