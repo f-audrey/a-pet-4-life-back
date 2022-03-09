0a7306a37717767ca7d7d25b9276375638df698f
Morgane-Rabiller <morganerabiller@yahoo.fr>
2022-03-09 15:21:55 +0100

    start home back-office

--- src/Models/Data.php
+++ src/Models/Data.php
@@ -6,21 +6,24 @@ class Data
 private $data = [
 
     [
-        'category_name' => 'Associations',
-        'picture' => 'associations.png',
-        'description' => 'Pour ajouter, modifer ou supprimer des associations.',
+        "category_name" => "Associations",
+        "picture" => "associations.png",
+        "description" => "Pour ajouter, modifer ou supprimer des associations.",
+        "path" => "{{ path('app_back_asso') }}",
     ],
 
     [
-      'category_name' => 'Particuliers',
-      'picture' => 'particuliers.png',
-      'description' => 'Pour ajouter, modifer ou supprimer les particuliers.',
+      "category_name" => "Particuliers",
+      "picture" => "particuliers.png",
+      "description" => "Pour ajouter, modifer ou supprimer les particuliers.",
+      "path" => "{{ path('app_back_particular') }}",
   ],
 
   [
-    'category_name' => 'Espèces',
-    'picture' => 'espèces.png',
-    'description' => 'Pour ajouter, modifer ou supprimer les espèces.',
+    "category_name" => "Espèces",
+    "picture" => "espèces.png",
+    "description" => "Pour ajouter, modifer ou supprimer les espèces.",
+    "path" => "{{ path('app_back_particular') }}",
 ],
 ];
 
